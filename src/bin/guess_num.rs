0f3af8f3ea7766e703cae06222e464a02bf201d1
SayMeeveTime <zhaomo0823@gmail.com>
2024-03-06 21:57:37 +0800

    guess_number

---
+++ src/bin/guess_num.rs
@@ -0,0 +1,37 @@
+use core::num;
+use std::io::{self, Read};
+use rand::Rng;
+
+fn main(){
+    println!("Guesss the number");
+    let secrect_number: u32 = rand::thread_rng().gen_range(1..101);
+    loop {
+        // 获取用户输入
+        println!("Please input your guess: ");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).unwrap();
+
+        let guess_number: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(err) => continue,
+        };
+
+        println!("You guesssed: {}",guess_number);
+
+        // 判断大小
+        if guess_number > secrect_number {
+            println!("Too big");
+        }else if guess_number < secrect_number {
+            println!("Too small");
+        }else {
+            // 判断正确的话  程序退出
+            println!("You win !");
+            break;
+        }
+    
+
+        
+
+    }
+
+}
