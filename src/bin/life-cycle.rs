8d92be7810762e71b7687ed5eb48ee738d6d01a2
SayMeeveTime <zhaomo0823@gmail.com>
2024-03-10 14:41:44 +0800

    life-cycle

---
+++ src/bin/life-cycle.rs
@@ -0,0 +1,11 @@
+fn bigger<'a>(str1: &'a str,str2: &'a str) -> &'a str { 
+    if  str1 > str2{
+        str1
+    }else {
+        str2
+    }
+}
+
+fn main(){
+    println!("{}",bigger("a", "b"));
+}
