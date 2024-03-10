9ecdf08375f13b962dd326e2bac7bbcb0260dba9
SayMeeveTime <zhaomo0823@gmail.com>
2024-03-10 14:06:07 +0800

    ownership

---
+++ src/bin/ownership.rs
@@ -0,0 +1,15 @@
+fn main(){
+
+    let s2: String;
+    { 
+        let s1 = String::from("Hello World");
+        let s2 = s1;
+        println!("{}",s1);
+    }
+    // println!("{}",s2);
+
+   
+
+
+
+}
